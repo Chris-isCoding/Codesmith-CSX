137dd588866af8d262f5aa96a847948b9c7ac263
Chris K <Chris-SourceCode@outlook.com>
2021-01-12 21:47:32 +0000

    add Challenge: While Loops - Fundamentals

---
+++ JavaScript-EasierParts/preCourse-Part2/while-loops-fundamentals.js
@@ -0,0 +1,23 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+Challenge: While Loops - Fundamentals
+A while loop is an alternative iteration statement. JavaScript's thread of execution will repeatedly process the code block until the while loop's conditional expression evaluates to false. **Be careful to avoid an infinite loop!**
+
+Challenge
+Use a while loop to increment count by 2 on each repetition of the block of code. Run the code block of your while loop until count is 8.
+
+*/
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+let count = 2;
+// ADD CODE HERE
+while (count < 8) {
+	count += 2;
+}
+
+// Uncomment these to check your work!
+console.log(count);
