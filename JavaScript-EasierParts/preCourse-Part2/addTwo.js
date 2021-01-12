5af67362497df47386e3d0ebdca5a0d65de65796
Chris K <Chris-SourceCode@outlook.com>
2021-01-12 22:44:57 +0000

    addd Challenge: addTwo

---
+++ JavaScript-EasierParts/preCourse-Part2/addTwo.js
@@ -0,0 +1,19 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+Challenge: addTwo
+Create a function addTwo that accepts a number, and returns the number plus 2.
+
+*/
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+function addTwo(num) {
+	return num + 2;
+}
+
+// Uncomment these to check your work!
+console.log(typeof addTwo); // should log: 'function'
+console.log(addTwo(10)); // should log: 12
