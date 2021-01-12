ad24d67d4b2a06467eee28452392d266d8ea4d6a
Chris K <Chris-SourceCode@outlook.com>
2021-01-12 21:36:32 +0000

    add Challenge: Arrays - Adding Elements

---
+++ JavaScript-EasierParts/preCourse-Part2/arrays-adding-elements.js
@@ -0,0 +1,18 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+// Challenge: Arrays - Adding Elements
+// Challenge
+// Use a built-in JavaScript method to add another show to the netflixShows array. Then log your updated array to the console.
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+const netflixShows = ['Orange is the New Black', 'Black Mirror', 'Chewing Gum'];
+
+// ADD CODE HERE
+let newShow = 'Coding Together';
+netflixShows.push(newShow);
+
+// Write a console.log statement below to check your work!
+console.log(netflixShows);
