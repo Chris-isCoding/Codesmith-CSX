88a38f3bb4218096594af90bf3ce06a624fd1855
Chris K <Chris-SourceCode@outlook.com>
2021-01-12 22:47:49 +0000

    add Challenge: sayHello

---
+++ JavaScript-EasierParts/preCourse-Part2/sayHello.js
@@ -0,0 +1,17 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+Challenge: sayHello
+Create a function sayHello that accepts a string argument and returns the string 'Hi, ' with the passed-in string added to the end.
+
+*/
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+const sayHello = (name) => 'Hi, ' + name;
+
+// Uncomment these to check your work!
+console.log(sayHello('Mary')); // should log: 'Hi, Mary'
+console.log(sayHello('Haley')); // should log: 'Hi, Haley'
