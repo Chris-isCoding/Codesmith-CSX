d4753f4b6f3a88511f4529cc9bddb0057145987f
Chris K <Chris-SourceCode@outlook.com>
2021-01-12 16:57:07 +0000

    add Challenge: Numbers - Multiplication/Division

---
+++ numbers-multiplication-division.js
@@ -0,0 +1,36 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+// Challenge: Numbers - Multiplication/Division
+// Numbers can also be multiplied and divided using the multiplication (*) and division (/) operators.
+
+// console.log(6 * 5); // => 30
+// console.log(45 / 5); // => 9
+// Challenge
+// Perform the following multiplication/division operations in the code editor:
+
+// 1. Multiply 20 by 4 and save the result to a variable called twentyTimesFour
+
+// 2. Divide 9 by 3 and save the result to a variable called nineOverThree
+
+// 3. Multiply 200.3 * 3 and save the result to a variable called twoHundredTimesThree
+
+// Floats
+// Any number with the decimal point is a float. Notice that in the last exercise you multiplied 200.3 by 3 and the output from that was 600.9000000000001. What happened here? Why didn't we get 600.9?
+
+// The way floats are handled in JavaScript can cause these rounding errors. Some decimal numbers cannot be represented accurately. The reason has to do with how numbers are encoded in JavaScript. It's something to be aware of when working with decimal numbers in JavaScript.
+
+// If you'd like to read more into the intricacies of float, click here.
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+// ADD CODE BELOW
+const twentyTimesFour = 20 * 4;
+const nineOverThree = 9 / 3;
+const twoHundredTimesThree = 200.3 * 3;
+
+// Uncomment these to check your work!
+console.log(twentyTimesFour);
+console.log(nineOverThree);
+console.log(twoHundredTimesThree);
