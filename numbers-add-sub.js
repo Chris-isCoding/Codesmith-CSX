6ffa4c34c2cf5c69bcc11cf13bd369036f56d24a
Chris K <Chris-SourceCode@outlook.com>
2021-01-12 16:53:39 +0000

    add Challenge: Numbers - Addition/Substraction

---
+++ numbers-add-sub.js
@@ -0,0 +1,34 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+// Challenge: Numbers - Addition/Subtraction
+// Another type of value that can be stored in a variable is a number. Pay close attention to the fact that the number is not wrapped in quotes. Keep in mind that if a value is wrapped in quotes, it becomes a string.
+
+// let age = 26;  // age is a number
+// let ageText = "26";  // ageText is a string
+// Numbers can be added or subtracted using the addition (+) and subtraction (-) operators.
+
+// let sum = 2 + 2;
+// console.log(sum); // should log: 4
+
+// let age = 30;
+// let tenYrsAgo = age - 10;
+// console.log(tenYrsAgo); // should log: 20
+// Challenge
+// Create a variable birthYear and set it equal to the year of your birth.
+
+// Next, create a variable named age and set the value to your current age.
+
+// Finally, create a variable named in10Yrs and set the value to equal your age plus 10.
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+// ADD CODE BELOW
+let birthYear = 1984;
+let age = 37;
+let in10Yrs = age + 10;
+
+// Uncomment these to check your work!
+console.log(birthYear);
+console.log(age);
+console.log(in10Yrs);
