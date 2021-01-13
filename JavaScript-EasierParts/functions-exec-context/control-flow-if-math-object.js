8a45cc04003aae6182fccbfb764c818cf4513707
Chris K <Chris-SourceCode@outlook.com>
2021-01-13 01:34:04 +0000

    tidy up layout

--- JavaScript-EasierParts/functions-exec-context/control-flow-if-math-object.js
+++ JavaScript-EasierParts/functions-exec-context/control-flow-if-math-object.js
@@ -5,7 +5,7 @@
 
  Using an IF/ELSE statement, write a function closestToTheMark that takes two player inputs as number arguments. The function will return 'Player 1 is closest' or 'Player 2 is closest' depending on which player input is closest to the randomly generated number.
 
- */
+*/
 
 // ========================== FIRST SOLUTION ==========================
 // ====================================================================
