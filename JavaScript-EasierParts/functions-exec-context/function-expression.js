fe311f410f7a9b9c65bd3154d2f1f11c63a251cd
Chris K <Chris-SourceCode@outlook.com>
2021-01-14 16:43:35 +0000

    add Challenge: Functions - Function Expression

---
+++ JavaScript-EasierParts/functions-exec-context/function-expression.js
@@ -0,0 +1,21 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+
+Convert the function named functionDeclaration to an anonymous function expression and assign it to a variable called myFunc.
+
+*/
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+// function functionDeclaration() {
+//   return "Hi there!";
+// }
+
+const myFunc = function () {
+	return 'Hi there!';
+};
+
+console.log(myFunc()); //should log: "Hi there!"
