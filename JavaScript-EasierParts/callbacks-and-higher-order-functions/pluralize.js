5bc0784a172713921031d823e03e32b12bd1807d
Chris K <Chris-SourceCode@outlook.com>
2021-01-15 21:47:33 +0000

    add Challenge: pluralize

---
+++ JavaScript-EasierParts/callbacks-and-higher-order-functions/pluralize.js
@@ -0,0 +1,25 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+
+Create a function pluralize that takes an array of strings as input and returns a new array with an "s" added to the end of each string in the input array. For example, if the string "carrot" is in the input array, it should become the string "carrots" in the output array.
+
+The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.
+
+*/
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+function pluralize(array) {
+	addS = [];
+	for (let i = 0; i < array.length; i++) {
+		addS.push(array[i] + 's');
+	}
+	return addS;
+}
+
+// Uncomment these to check your work!
+const animals = ['dog', 'cat', 'tree frog'];
+console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
