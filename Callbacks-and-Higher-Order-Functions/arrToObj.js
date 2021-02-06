611447b954f43dfacb62c6e464c20a2f78dae1f4
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-02-06 01:04:19 +0000

    add Challenge: arrToObj

---
+++ Callbacks-and-Higher-Order-Functions/arrToObj.js
@@ -0,0 +1,25 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+
+Challenge: arrToObj
+Add code to the function arrToObj in the place marked "ADD CODE HERE" in order to achieve the desired console logs. arrToObj should return an object that has elements from the passed-in array as keys, and the outputs from the callback (when those elements are passed in) as the corresponding values.
+
+*/
+
+// ========================== SOLUTION ======================================
+// ================================================================================
+
+function arrToObj(array, callback) {
+	const obj = {};
+	for (let ele in array) {
+		obj[array[ele]] = callback(array[ele]);
+	}
+	return obj;
+}
+
+// Uncomment these to check your work!
+const arrOfStrings = ['beer', 'glue'];
+const capitalize = (str) => str.toUpperCase();
+console.log(arrToObj(arrOfStrings, capitalize)); // should log: { beer: 'BEER', glue: 'GLUE' }
