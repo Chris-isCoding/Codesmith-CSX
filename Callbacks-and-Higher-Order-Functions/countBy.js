ceb9ccaf678f3104c5902fea13abcdf8dec3cdfa
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-02-17 17:31:50 +0000

    add solution to Challenge: countBy

--- Callbacks-and-Higher-Order-Functions/countBy.js
+++ Callbacks-and-Higher-Order-Functions/countBy.js
@@ -35,13 +35,94 @@ Create a function countBy that accepts an array and a callback, and returns an o
 // ========================== SOLUTION 2 ======================================
 // ================================================================================
 
+// const countBy = (arr, callback) =>
+// 	arr
+// 		.map((ele) => callback(ele))
+// 		.reduce((acc, ele) => {
+// 			acc[ele] = acc[ele] ? acc[ele] + 1 : 1;
+// 			return acc;
+// 		}, {});
+
+// ========================== SOLUTION 3 ======================================
+// ================================================================================
+
+// function countBy(array, callback) {
+//   const result = {};
+//   array.forEach((num) => {
+//     result[callback(num)] = result[callback(num)]
+//       ? result[callback(num)] + 1
+//       : 1;
+//   });
+//   return result;
+// }
+
+// ========================== SOLUTION 4 ======================================
+// ================================================================================
+
+// function countBy(array, callback) {
+//   const obj = {};
+//   for (const ele of array) {
+//     obj[callback(ele)] = obj[callback(ele)] ? obj[callback(ele)] + 1 : 1;
+//   }
+//   return obj;
+// }
+
+// ========================== SOLUTION 5 ======================================
+// ================================================================================
+
+// function countBy(array, callback) {
+//   const obj = {};
+//   for (const ele of array) {
+//     const key = callback(ele);
+//     obj[key] = obj[key] ? obj[key] + 1 : 1;
+//   }
+//   return obj;
+// }
+
+// ========================== SOLUTION 6 ======================================
+// ================================================================================
+
+// function countBy(array, callback) {
+//   const obj = {};
+//   array.forEach(ele => {
+//     const key = callback(ele);
+//     obj[key] = obj[key] ? obj[key] + 1 : 1;
+//   });
+//   return obj;
+// }
+
+// ========================== SOLUTION 7 ======================================
+// ================================================================================
+
+// function countBy(array, callback) {
+//   const obj = {};
+//   array.map(ele => callback(ele)).forEach(key => (obj[key] = 1 + (obj[key] || 0)))
+//   return obj;
+// }
+
+// ========================== SOLUTION 8 ======================================
+// ================================================================================
+
 const countBy = (arr, callback) =>
-	arr
-		.map((ele) => callback(ele))
-		.reduce((acc, ele) => {
-			acc[ele] = acc[ele] ? acc[ele] + 1 : 1;
-			return acc;
-		}, {});
+	arr.reduce((acc, ele) => {
+		const key = callback(ele);
+		if (acc[key] === undefined) {
+			acc[key] = 1;
+		} else {
+			acc[key]++;
+		}
+		// acc[key] = acc[key] ? acc[key] + 1 : 1;
+		return acc;
+	}, {});
+
+// Uncomment these to check your work!
+function evenOdd(n) {
+	console.log(n);
+	if (n % 2 === 0) return 'even';
+	else return 'odd';
+}
+const nums = [1, 2, 3, 4, 5];
+console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
 
 // Uncomment these to check your work!
 function evenOdd(n) {
