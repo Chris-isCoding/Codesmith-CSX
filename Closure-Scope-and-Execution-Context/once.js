c827c8ac040f3d577936a4922b857b37f3e3a586
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-02-27 09:54:44 +0000

    add Challenge: once

---
+++ Closure-Scope-and-Execution-Context/once.js
@@ -0,0 +1,36 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+
+Challenge: once
+
+Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
+*/
+
+// ========================== SOLUTION 1 ======================================
+// ================================================================================
+
+function once(callback) {
+	const counter = [];
+	function returnedFunc(...arg) {
+		if (counter.length === 0) {
+			// console.log(callback(...arg))
+			counter.push(callback(...arg));
+		}
+		return counter[0];
+	}
+	return returnedFunc;
+}
+
+const addByTwoOnce = once(function (num) {
+	return num + 2;
+});
+
+// ========================== SOLUTION 1 ======================================
+// ================================================================================
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+console.log(addByTwoOnce(5)); //should log 7
+console.log(addByTwoOnce(10)); //should log 7
+console.log(addByTwoOnce(9001)); //should log 7
