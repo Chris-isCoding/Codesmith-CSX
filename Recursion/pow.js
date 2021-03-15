b06f6754b9c730bfe27d7e6dfa9157c72740e576
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-03-15 23:45:30 +0000

    add Challenge: POW

---
+++ Recursion/pow.js
@@ -0,0 +1,38 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+
+Challenge: POW
+
+Write a function that takes two inputs, a base and an exponent, and returns the expected value of base ^ exponent. For instance, if our base is 2 and our exponent is 3, then return 8 because 2^3 = 2*2*2 = 8.
+
+Input 1: {Number} base - base number raised to the exponent
+Input 2: {Number} exponent - exponent the base is raised to
+Output: {Number} - expected value of base raised to exponent
+
+
+*/
+
+// ========================== SOLUTION 1 ======================================
+// ================================================================================
+
+// function pow(base, exponent) {
+//   if (exponent === 0) return 1;
+//   if (exponent < 0) return (1 / (base * pow(base, -exponent - 1)))
+//   return base * pow(base, exponent - 1)
+// }
+
+// ========================== SOLUTION 2 ======================================
+// ================================================================================
+
+const pow = (base, exponent) =>
+	exponent === 0
+		? 1
+		: exponent > 0
+		? base * pow(base, exponent - 1)
+		: 1 / (base * pow(base, -exponent - 1));
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(pow(2, 1)); // -> 16
+console.log(pow(3, 5)); // -> 243
