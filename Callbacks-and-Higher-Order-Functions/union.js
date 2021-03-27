c435eb27055b3e9278ae6d8bdd8b1ccde9c6f03f
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-03-27 11:02:38 +0000

    refactor solutions to Challenge: union

--- Callbacks-and-Higher-Order-Functions/union.js
+++ Callbacks-and-Higher-Order-Functions/union.js
@@ -13,22 +13,18 @@ Construct a function union that takes an input array of arrays, compares each ar
 // ================================================================================
 
 // function union(arrOfArrays) {
-//   const oneArray = arrOfArrays.reduce(function (accumulatorArr, currentArray) {
-//     return accumulatorArr.concat(currentArray);
-//   }, []);
-//   // console.log(oneArray);
-//   const removedDuplicatesArray = oneArray.reduce(function (
-//     accumulatorArr,
-//     currentElement
-//   ) {
-//     if (accumulatorArr.indexOf(currentElement) === -1) {
-//       // if element does not repeat
-//       accumulatorArr.push(currentElement); // push it to accumulatorArr
-//     }
-//     return accumulatorArr;
-//   },
-//   []); // empty arr as a initialValue
-//   return removedDuplicatesArray;
+// 	const oneArray = arrOfArrays.reduce(function (accumulatorArr, currentArray) {
+// 		return accumulatorArr.concat(currentArray);
+// 	}, []);
+// 	// console.log(oneArray);
+// 	const removedDuplicatesArray = oneArray.reduce(function (accumulatorArr, currentElement) {
+// 		if (accumulatorArr.indexOf(currentElement) === -1) {
+// 			// if element does not repeat
+// 			accumulatorArr.push(currentElement); // push it to accumulatorArr
+// 		}
+// 		return accumulatorArr;
+// 	}, []); // empty arr as a initialValue
+// 	return removedDuplicatesArray;
 // }
 
 // ========================== SOLUTION 2 ======================================
@@ -37,7 +33,7 @@ Construct a function union that takes an input array of arrays, compares each ar
 // const union = function(arrays) {
 //   const flattened = arrays.reduce(function (acc, currVal) {
 //     return acc.concat(currVal);
-//   }, []);
+//   });
 //   const filtered = flattened.filter(function (cur, index) {
 //     return flattened.indexOf(cur) === index;
 //   });
@@ -51,32 +47,60 @@ Construct a function union that takes an input array of arrays, compares each ar
 // 	const result = arrOfArrays
 // 		.reduce(function (accumulatorArr, currentArray) {
 // 			return accumulatorArr.concat(currentArray);
-// 		}, [])
+// 		})
 // 		.reduce(function (accumulatorArr, currentElement) {
 // 			if (accumulatorArr.indexOf(currentElement) === -1) {
 // 				// if element does not repeat
 // 				accumulatorArr.push(currentElement); // push it to accumulatorArr
 // 			}
 // 			return accumulatorArr;
-// 		}, []); // empty arr as a initialValue
+// 		}, arrOfArrays[0]); // first arr as a initialValue
 // 	return result;
 // }
 
 // ========================== SOLUTION 4 ======================================
 // ================================================================================
 
-// const union = arrays => arrays.reduce((acc, cur, ) => acc.concat(cur), [])
-//   .reduce((arr, ele) => {
+// const union = arrays => arrays.reduce((acc, cur) => acc.concat(cur)).reduce((arr, ele) => {
 //     if (arr.indexOf(ele) === -1) {
 //       arr.push(ele);
 //     }
 //     return arr
-//   }, []);
+//   }, arrays[0]); // first array in arrays
 
 // ========================== SOLUTION 5 ======================================
 // ================================================================================
 
-const union = (arrays) => [...new Set(arrays.flat())];
+// const union = arrays => [...new Set(arrays.flat())];
+
+// ========================== SOLUTION 6 ======================================
+// ================================================================================
+
+function union(arrays) {
+	return arrays.reduce(function (accum, cur) {
+		// console.log(cur)
+		cur.forEach(function (el) {
+			if (accum.includes(el) === false) {
+				accum.push(el);
+				// console.log(accum)
+			}
+		});
+		return accum;
+	});
+}
+
+// ========================== SOLUTION 7 ======================================
+// ================================================================================
+
+const union = (arrays) =>
+	arrays.reduce((acc, cur) => {
+		cur.forEach((el) => {
+			if (!acc.includes(el)) {
+				acc.push(el);
+			}
+		});
+		return acc;
+	});
 
 // // Uncomment these to check your work!
 const arr1 = [5, 10, 15];
