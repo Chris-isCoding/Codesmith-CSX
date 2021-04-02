dbff0da899b73e4ae0bd29d5a8eb0a5140e7f093
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-02 15:49:15 +0100

    add Challenge: personStore.js

---
+++ personStore.js
@@ -0,0 +1,19 @@
+// ============================== INSTRUCTIONS ==============================
+// ================================================================================
+
+/*
+
+Challenge: personStore
+Inside personStore object, create a property greet where the value is a function that logs "hello".
+
+*/
+
+// ========================== SOLUTION 1 ======================================
+// ================================================================================
+
+const personStore = {
+	greet: () => console.log('hello'),
+};
+
+// Uncomment this line to check your work!
+personStore.greet(); // -> Logs 'hello'
