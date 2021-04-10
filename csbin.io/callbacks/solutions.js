3f222d590809a1a1596785db8c2944cfc0a5f810
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-10 12:37:52 +0100

    add Challenge: 1

---
+++ csbin.io/callbacks/solutions.js
@@ -0,0 +1,18 @@
+// console.log('Hello, world!');
+
+// ============================== CHALLENGE 1  ==============================
+// ==========================================================================
+
+/*
+
+Create a function addTwo that accepts one input and adds 2 to it.
+
+*/
+
+function addTwo(num) {
+	return num + 2;
+}
+
+// To check if you've completed it, uncomment these console.logs!
+console.log(addTwo(3)); // => 5
+console.log(addTwo(10)); // => 12
