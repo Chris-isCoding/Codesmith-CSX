b6a2dbd668171c62b32919b881870358872e9e39
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-10 12:38:45 +0100

    add Challenge: 2

--- csbin.io/callbacks/solutions.js
+++ csbin.io/callbacks/solutions.js
@@ -14,5 +14,22 @@ function addTwo(num) {
 }
 
 // To check if you've completed it, uncomment these console.logs!
-console.log(addTwo(3)); // => 5
-console.log(addTwo(10)); // => 12
+// console.log(addTwo(3)); // => 5
+// console.log(addTwo(10)); // => 12
+
+// ============================== CHALLENGE 2  ==============================
+// ==========================================================================
+
+/*
+
+Create a function addS that accepts one input and adds an "s" to it.
+
+*/
+
+function addS(word) {
+	return word + 's';
+}
+
+// uncomment these to check your work
+console.log(addS('pizza')); // => pizzas
+console.log(addS('bagel')); // => bagels
