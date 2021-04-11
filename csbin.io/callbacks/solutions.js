b888847ce368a8675c780b777e4c71ace6786edc
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-11 15:59:31 +0100

    add csbin.io/callbacks Challenge: 09 - objOfMatches

--- csbin.io/callbacks/solutions.js
+++ csbin.io/callbacks/solutions.js
@@ -230,3 +230,37 @@ function union(...arrays) {
 
 console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
+
+// ============================== CHALLENGE 9  ==============================
+// ==========================================================================
+
+/*
+
+Construct a function objOfMatches that accepts two arrays and a callback. objOfMatches will build an object and return it. To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
+
+*/
+
+// ============================== SOLUTION 1  ==============================
+// ==========================================================================
+
+// function objOfMatches(array1, array2, callback) {
+//   const obj = {};
+//   for (const i in array1) {
+//     if (callback(array1[i]) === array2[i]) {
+//       obj[array1[i]] = array2[i]
+//     }
+//   }
+//   return obj;
+// }
+
+// ============================== SOLUTION 2  ==============================
+// ==========================================================================
+
+const objOfMatches = (arr1, arr2, cb) => arr1.reduce((acc, cur, i) => (cb(cur) === arr2[i] ? ((acc[cur] = arr2[i]), acc) : acc), {});
+
+console.log(
+	objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function (str) {
+		return str.toUpperCase();
+	})
+);
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
