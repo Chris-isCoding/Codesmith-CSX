10a208475bbc92ca766a65d65b7ddb2be0db9ee0
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-17 11:47:59 +0100

    add csbin.io/callbacks Challenge: 17 - commutative

--- csbin.io/callbacks/solutions.js
+++ csbin.io/callbacks/solutions.js
@@ -572,7 +572,8 @@ const sunny = { mac: 'priest', dennis: 'calculating', charlie: 'birdlaw', dee: '
 const startsWithBird = function (str) {
 	return str.slice(0, 4).toLowerCase() === 'bird';
 };
-console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
+
+// console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
 
 // ============================== CHALLENGE 17  ==============================
 // ==========================================================================
@@ -583,15 +584,20 @@ Create a function commutative that accepts two callbacks and a value. commutativ
 
 */
 
-function commutative(func1, func2, value) {}
+// ============================== SOLUTION 1 ==============================
+// ==========================================================================
+
+function commutative(func1, func2, value) {
+	return func2(func1(value)) === func1(func2(value));
+}
 
 // /*** Uncomment these to check your work! ***/
-// const multBy3 = n => n * 3;
-// const divBy4 = n => n / 4;
-// const subtract5 = n => n - 5;
-// console.log(commutative(multBy3, divBy4, 11)); // should log: true
-// console.log(commutative(multBy3, subtract5, 10)); // should log: false
-// console.log(commutative(divBy4, subtract5, 48)); // should log: false
+const multBy3 = n => n * 3;
+const divBy4 = n => n / 4;
+const subtract5 = n => n - 5;
+console.log(commutative(multBy3, divBy4, 11)); // should log: true
+console.log(commutative(multBy3, subtract5, 10)); // should log: false
+console.log(commutative(divBy4, subtract5, 48)); // should log: false
 
 // ============================== CHALLENGE 18 =============================
 // ==========================================================================
