72a7e0e81da43018ee0431eba9bcd242a5f69fac
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-17 12:30:10 +0100

    add csbin.io/callbacks Challenge: 19 - rating

--- csbin.io/callbacks/solutions.js
+++ csbin.io/callbacks/solutions.js
@@ -641,16 +641,27 @@ Create a function rating that accepts an array (of functions) and a value. All t
 
 */
 
-function rating(arrOfFuncs, value) {}
+// ============================== SOLUTION 1 ==============================
+// ==========================================================================
+
+// function rating(arrOfFuncs, value) {
+// 	return (arrOfFuncs.filter(func => func(value)).length / arrOfFuncs.length) * 100;
+// }
+
+// ============================== SOLUTION 2 ==============================
+// ==========================================================================
+
+const rating = (funcs, value) => (funcs.filter(func => func(value)).length / funcs.length) * 100;
 
 // /*** Uncomment these to check your work! ***/
-// const isEven = n => n % 2 === 0;
-// const greaterThanFour = n => n > 4;
-// const isSquare = n => Math.sqrt(n) % 1 === 0;
-// const hasSix = n => n.toString().includes('6');
-// const checks = [isEven, greaterThanFour, isSquare, hasSix];
-// console.log(rating(checks, 64)); // should log: 100
-// console.log(rating(checks, 66)); // should log: 75
+const isEven = n => n % 2 === 0;
+const greaterThanFour = n => n > 4;
+const isSquare = n => Math.sqrt(n) % 1 === 0;
+const hasSix = n => n.toString().includes('6');
+const checks = [isEven, greaterThanFour, isSquare, hasSix];
+
+console.log(rating(checks, 64)); // should log: 100
+console.log(rating(checks, 66)); // should log: 75
 
 // ============================== CHALLENGE 20  ==============================
 // ==========================================================================
