2aa5c8950921be98ccac39791dea4b87803223ac
Chris-SourceCode <Chris-SourceCode@outlook.com>
2021-04-22 18:32:39 +0100

    add csbin.io/closures Challenge: 15 - russianRoulette

--- csbin.io/closures/closures.js
+++ csbin.io/closures/closures.js
@@ -491,10 +491,10 @@ const callTimes = () => {
 
 let myNewFunc1 = callTimes();
 let myNewFunc2 = callTimes();
-console.log(myNewFunc1()); // => 1
-console.log(myNewFunc1()); // => 2
-console.log(myNewFunc2()); // => 1
-console.log(myNewFunc2()); // => 2
+// console.log(myNewFunc1()); // => 1
+// console.log(myNewFunc1()); // => 2
+// console.log(myNewFunc2()); // => 1
+// console.log(myNewFunc2()); // => 2
 
 // /*** Uncomment these to check your work! ***/
 // let myNewFunc1 = callTimes();
@@ -516,15 +516,22 @@ Create a function russianRoulette that accepts a number (let us call it n), and 
 // ============================== SOLUTION 1 ==============================
 // ==========================================================================
 
-function russianRoulette(num) {}
+function russianRoulette(num) {
+	return function () {
+		--num;
+		if (num > 0) return 'click';
+		if (num === 0) return 'bang';
+		if (num < 0) return 'reload to play again';
+	};
+}
 
 // /*** Uncomment these to check your work! ***/
 const play = russianRoulette(3);
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'bang'
-// console.log(play()); // => should log 'reload to play again'
-// console.log(play()); // => should log 'reload to play again'
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'bang'
+console.log(play()); // => should log 'reload to play again'
+console.log(play()); // => should log 'reload to play again'
 
 // ============================== CHALLENGE 16  ==============================
 // ==========================================================================
