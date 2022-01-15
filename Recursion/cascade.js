603252a245fc438b799ebf260705d10891368eca
Chris K <Chris-SourceCode@outlook.com>
2022-01-15 23:57:55 +0000

    feat: Add new solution to cascade challenge

--- Recursion/cascade.js
+++ Recursion/cascade.js
@@ -21,24 +21,41 @@ cascade(12345) should print
 */
 
 // ========================== SOLUTION 1 ======================================
-// ================================================================================
+// =============================================================================
 
-function cascade(number) {
-  if (!Number.isInteger(number) || number < 0) return;
-  const stringified = number.toString();
-  const cascadeDecreasing = (i) => {
-    if (i === 0) return;
-    console.log(Number(stringified.slice(0, i)));
-    cascadeDecreasing(i - 1);
-  };
-  const cascadeIncreasing = (i) => {
-    if (i > stringified.length) return;
-    console.log(Number(stringified.slice(0, i)));
-    cascadeIncreasing(i + 1);
-  };
-  cascadeDecreasing(stringified.length);
-  cascadeIncreasing(2);
-}
+// function cascade(number) {
+//   if (!Number.isInteger(number) || number < 0) return;
+//   const stringified = number.toString();
+//   const cascadeDecreasing = (i) => {
+//     if (i === 0) return;
+//     console.log(Number(stringified.slice(0, i)));
+//     cascadeDecreasing(i - 1);
+//   };
+//   const cascadeIncreasing = (i) => {
+//     if (i > stringified.length) return;
+//     console.log(Number(stringified.slice(0, i)));
+//     cascadeIncreasing(i + 1);
+//   };
+//   cascadeDecreasing(stringified.length);
+//   cascadeIncreasing(2);
+// }
+
+// // Uncomment to test your work!
+// cascade(12345);
+
+// ========================== SOLUTION 2 ======================================
+// =============================================================================
+
+const cascade = (num) => {
+  if (!Number.isInteger(num) || num < 0) return;
+  if (num < 10) {
+    console.log(num);
+    return;
+  }
+  console.log(num);
+  cascade(Math.trunc(num / 10));
+  console.log(num);
+};
 
 // Uncomment to test your work!
 cascade(12345);
