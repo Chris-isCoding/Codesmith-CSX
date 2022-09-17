23d3c437b90d8d72444d5067c43025bd252d7fa4
Chris K <Chris-SourceCode@outlook.com>
2022-09-17 15:46:55 +0200

    feat: Add additional solution for reduce

--- Callbacks-and-Higher-Order-Functions/reduce.js
+++ Callbacks-and-Higher-Order-Functions/reduce.js
@@ -1,5 +1,5 @@
-// ============================== INSTRUCTIONS ==============================
-// ================================================================================
+// ============================== INSTRUCTIONS =============================
+// =============================================================================
 
 /*
 
@@ -22,81 +22,137 @@ Construct your own reduce function that accepts an array, a callback, and an ini
 
 */
 
-// ========================== SOLUTION 1 ======================================
-// ================================================================================
+// ========================== SOLUTION 1 =====================================
+// =============================================================================
 
 // function reduce(array, callback, initialValue) {
-// 	let accumulatorValue = initialValue;
-// 	for (let i = 0; i < array.length; i++) {
-// 		accumulatorValue = callback(accumulatorValue, array[i]);
-// 	}
-// 	return accumulatorValue;
+//   let accumulatorValue = initialValue;
+//   for (let i = 0; i < array.length; i++) {
+//     accumulatorValue = callback(accumulatorValue, array[i]);
+//   }
+//   return accumulatorValue;
 // }
 
 // // Uncomment these to check your work!
 // const nums = [4, 1, 3];
 // const add = function (a, b) {
-// 	return a + b;
+//   return a + b;
 // };
 // console.log(reduce(nums, add, 0)); // should log 8
 
-// ========================== SOLUTION 2 ======================================
-// ================================================================================
+// ========================== SOLUTION 2 =====================================
+// =============================================================================
 
 // function reduce(array, callback, initialValue) {
-// 	if (Array.isArray(array)) {
-// 		let accumulatorValue;
-// 		if (initialValue === 'undefined') {
-// 			accumulatorValue = arrray[0];
-// 			for (let i = 1; i < array.length; i++) {
-// 				accumulatorValue = callback(accumulatorValue, array[i]);
-// 				return accumulatorValue;
-// 			}
-// 		}
-// 		accumulatorValue = initialValue;
-// 		for (let i = 0; i < array.length; i++) {
-// 			accumulatorValue = callback(accumulatorValue, array[i]);
-// 		}
-// 		return accumulatorValue;
-// 	}
-// 	return 'Please pass an array as the first argument';
+//   if (Array.isArray(array)) {
+//     let accumulatorValue;
+//     if (initialValue === 'undefined') {
+//       accumulatorValue = arrray[0];
+//       for (let i = 1; i < array.length; i++) {
+//         accumulatorValue = callback(accumulatorValue, array[i]);
+//         return accumulatorValue;
+//       }
+//     }
+//     accumulatorValue = initialValue;
+//     for (let i = 0; i < array.length; i++) {
+//       accumulatorValue = callback(accumulatorValue, array[i]);
+//     }
+//     return accumulatorValue;
+//   }
+//   return 'Please pass an array as the first argument';
 // }
 
 // // Uncomment these to check your work!
 // const nums = [4, 1, 3];
 // const add = function (a, b) {
-// 	return a + b;
+//   return a + b;
 // };
 
 // console.log(reduce(nums, add, 0)); // should log 8
 
-// ========================== SOLUTION 3 ======================================
-// ================================================================================
+// ========================== SOLUTION 3 =====================================
+// =============================================================================
 
-function reduce(array, callback, initialValue) {
-	if (Array.isArray(array)) {
-		let accumulatorValue;
-		let i;
-		if (initialValue === 'undefined') {
-			accumulatorValue = arrray[0];
-			i = 1;
-		} else {
-			//often we can skip else but in the case like this is mandatory for our code to execute correctly
-			i = 0;
-			accumulatorValue = initialValue;
-		}
-		for (; i < array.length; i++) {
-			accumulatorValue = callback(accumulatorValue, array[i]);
-		}
-		return accumulatorValue;
-	}
-	return 'Please pass an array as the first argument';
+// function reduce(array, callback, initialValue) {
+//   if (Array.isArray(array)) {
+//     let accumulatorValue;
+//     let i;
+//     if (initialValue === 'undefined') {
+//       [accumulatorValue] = array;
+//       i = 1;
+//     } else {
+//       // often we can skip else but in the case like this is mandatory for our code to execute correctly
+//       i = 0;
+//       accumulatorValue = initialValue;
+//     }
+//     for (; i < array.length; i++) {
+//       accumulatorValue = callback(accumulatorValue, array[i]);
+//     }
+//     return accumulatorValue;
+//   }
+//   return 'Please pass an array as the first argument';
+// }
+
+// ========================== SOLUTION 4 =====================================
+// =============================================================================
+// following ECMAScript specification
+
+function reduce(...args) {
+  validateInput(args);
+  const [array, callback, initialValue] = args;
+  if (array.length === 0 && args.length >= 3) {
+    return initialValue;
+  }
+
+  let i = 0;
+  let accumulator;
+  if (args.length > 2) {
+    accumulator = initialValue;
+  } else {
+    while (!(i in array) && i < array.length) {
+      if (i === array.length - 1) {
+        throw new Error('Reduce of empty array with no initial value');
+      }
+      i++;
+    }
+    if (i in array) {
+      accumulator = array[i];
+      i++;
+    }
+  }
+  while (i < array.length) {
+    if (i in array) {
+      accumulator = callback(accumulator, array[i]);
+    }
+    i++;
+  }
+  return accumulator;
+}
+
+function validateInput(args) {
+  if (args.length <= 1) {
+    throw new Error('Missing argument(s)');
+  }
+
+  const [array, callback] = args;
+
+  if (args.length > 1) {
+    if (Array.isArray(array) === false) {
+      throw new TypeError(`Expected an array, received ${array}`);
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError(`${callback} is not a function`);
+    }
+  }
+  if (array.length === 0 && args.length < 3) {
+    throw new Error('Reduce of an empty array with no initial value');
+  }
 }
 
 // Uncomment these to check your work!
 const nums = [4, 1, 3];
 const add = function (a, b) {
-	return a + b;
+  return a + b;
 };
 
 console.log(reduce(nums, add, 0)); // should log 8
